df7de97c7ad95c4e64a93781a801a1fd79f65aa1
VIGGY2582 <vigneshwarachakravarthy@gmail.com>
2026-03-02 22:40:02 +0530

    Sort LL

--- Arrays/2_pointer/Sort_a_ll.java
+++ Arrays/2_pointer/Sort_a_ll.java
@@ -26,15 +26,102 @@ The number of nodes in the list is in the range [0, 5 * 104].
 
 Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?
 */
+import java.util.*;
 
-class Solution {
-        public ListNode sortList(ListNode head) {
-                if (head == null || head.next == null) {
-                            return head;
-                                    }
+class Main {
 
-                                            // Step 1: Find middle
-                                                    ListNode slow = head;
-                                                            ListNode fast = head;
-                                                                    ListNode prev = null;
-…                                                                       }
+    // Definition for singly-linked list
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();  // number of nodes
+
+        if (n == 0) {
+            System.out.println();
+            return;
+        }
+
+        // Create linked list
+        ListNode head = new ListNode(sc.nextInt());
+        ListNode current = head;
+
+        for (int i = 1; i < n; i++) {
+            current.next = new ListNode(sc.nextInt());
+            current = current.next;
+        }
+
+        // Sort the linked list
+        head = sortList(head);
+
+        // Print sorted list
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+
+        sc.close();
+    }
+
+    // Merge Sort for Linked List
+    public static ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        // Find middle
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Break list into two halves
+        prev.next = null;
+
+        // Recursively sort both halves
+        ListNode left = sortList(head);
+        ListNode right = sortList(slow);
+
+        // Merge sorted halves
+        return merge(left, right);
+    }
+
+    // Merge two sorted lists
+    public static ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+
+        if (l1 != null) {
+            tail.next = l1;
+        } else {
+            tail.next = l2;
+        }
+
+        return dummy.next;
+    }
+}
